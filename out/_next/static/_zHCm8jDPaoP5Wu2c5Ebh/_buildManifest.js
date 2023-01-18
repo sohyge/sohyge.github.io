689af2428934f0fc85705ae842fa98c3352c0089
HyukGeun Song <wck_os12@wconcept.co.kr>
2023-01-18 12:13:25 +0900

    deploy to gh-pages

---
+++ out/_next/static/_zHCm8jDPaoP5Wu2c5Ebh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cb64ffc1988dbddb.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
